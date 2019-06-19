5a6ee1c559c8e4239956dd5eb9b66df445a610ac
Wioleta Budny <wiolamar@poczta.onet.pl>
2019-06-19 14:54:15 +0200

    czesc zadania

--- zadanie_4/zad_42-WB.py
+++ zadanie_4/zad_42-WB.py
@@ -1,2 +1,10 @@
 # zadanie 4.2
-print('hehja')
+def silnia_rek(n):
+    """Obliczanie silni rekurencyjnie"""
+    if n>1:
+        return n*silnia_rek(n-1)
+    elif n in (0,1):
+        return 1
+
+
+print(silnia_rek(7))
